71112d893e293356d06f1f94c8c0a4f81afb8c35
Crowdbotics <team@crowdbotics.com>
2023-10-25 04:16:33 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "spring-glade-44134",
+  url: "https://spring-glade-44134.botics.co",
+  api: "https://spring-glade-44134.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
